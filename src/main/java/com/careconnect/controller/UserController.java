7346341ed5e756b7d07e8ea37392b3a62e74317c
tulasi <gogisettytulasi@gmail.com>
2026-04-29 18:15:01 +0530

    vercel deployment

--- src/main/java/com/careconnect/controller/UserController.java
+++ src/main/java/com/careconnect/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://careconnect-eight-chi.vercel.app")
 public class UserController {
 
     private final UserRepository userRepository;
